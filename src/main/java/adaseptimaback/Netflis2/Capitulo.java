484a75c9f5537da1d7df77e74f684383a6c7f1bb
vickybilla <jimenezvictoria@hotmail.com>
2019-10-09 15:13:43 -0300

    agrego modif que conectan capitulo con genero de serie

--- src/main/java/adaseptimaback/Netflis2/Capitulo.java
+++ src/main/java/adaseptimaback/Netflis2/Capitulo.java
@@ -3,21 +3,28 @@ package adaseptimaback.Netflis2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Capitulo extends UnidadDeContenido {
 
+public class Capitulo extends UnidadDeContenido {
     private Integer numeroDeCapitulo;
-    private List<Actor> actoresInvitados= new ArrayList<>();
+    private List <Actor> actoresInvitados= new ArrayList<>();
     private Integer duracionDeCapitulo;
     private Temporada temporada;
     private Serie serie;
 
-    public Capitulo(Integer numeroDeCapitulo, Integer duracionDeCapitulo, Temporada temporada){
-        super();
+
+    public String generoDeSerie(){
+        return this.getSerie().getGenero();
+    }
+
+
+    public Capitulo(Integer numeroDeCapitulo, Integer duracionDeCapitulo, Temporada temporada, Serie serie){
         this.numeroDeCapitulo=numeroDeCapitulo;
         this.duracionDeCapitulo=duracionDeCapitulo;
         this.temporada=temporada;
-
+        this.setSerie(serie);
+        this.setGenero(generoDeSerie());
     }
+
 
     public void setActoresInvitados (Actor unActorInvitado){
 
@@ -44,5 +51,13 @@ public class Capitulo extends UnidadDeContenido {
     public void setNumeroDeCapitulo(Integer numeroDeCapitulo) {
 
         this.numeroDeCapitulo = numeroDeCapitulo;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
     }
 }
