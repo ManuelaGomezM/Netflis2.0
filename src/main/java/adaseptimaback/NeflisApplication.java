2846b269fdfd250ef04cbb60dde48b844f98273a
vickybilla <jimenezvictoria@hotmail.com>
2019-10-09 17:16:19 -0300

    deje un solo main

--- src/main/java/adaseptimaback/NeflisApplication.java
+++ src/main/java/adaseptimaback/NeflisApplication.java
@@ -3,7 +3,6 @@ package adaseptimaback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
     @SpringBootApplication
     public class NeflisApplication {
         public static void main(String[] args) {
